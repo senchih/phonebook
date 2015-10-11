1f26ec6cf4e6f520aee2bb18aefbd0429452e2b9
senchih <a2215689@yahoo.com.tw>
2015-10-11 23:17:03 +0800

    Fix bad code
    1. fix the name (data->content) not to make confused name.
    2. delete dead code

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -1,17 +1,19 @@
 #ifndef _PHONEBOOK_H
 #define _PHONEBOOK_H
 
+#ifndef MAX_LAST_NAME_SIZE
 #define MAX_LAST_NAME_SIZE 16
+#endif
 
 /* original version */
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
     struct __PHONE_BOOK_ENTRY *pNext;
-    struct __PHONE_BOOK_DATA *pData;
+    struct __PHONE_BOOK_CONTENT *pContent;
 } entry;
 
-typedef struct __PHONE_BOOK_DATA {
-    char lastName[MAX_LAST_NAME_SIZE];
+typedef struct __PHONE_BOOK_CONTENT {
+    struct __PHONE_BOOK_ENTRY *pLastname;
     char firstName[16];
     char email[16];
     char phone[10];
@@ -21,8 +23,7 @@ typedef struct __PHONE_BOOK_DATA {
     char city[16];
     char state[2];
     char zip[5];
-    //struct __PHONE_BOOK_ENTRY *pNext;
-} data;
+} content;
 
 entry *findName(char lastname[], entry *pHead);
 entry *append(char lastName[], entry *e);
